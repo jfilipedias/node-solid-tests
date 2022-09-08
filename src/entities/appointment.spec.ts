0102ddead9faadfe9231371a032ba99736024197
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-09-08 18:15:13 -0300

    Adds new rule and test to appointment entity

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -17,11 +17,27 @@ test("Create an appointment", () => {
   expect(appointment.customer).toEqual("John Doe")
 })
 
-test("Cannot create an appointment with end date before start date", () => {
+test("Cannot create an appointment with end date before the start date", () => {
   const startsAt = new Date()
   const endsAt = new Date()
 
   endsAt.setDate(endsAt.getDate() - 1)
+
+  expect(() => {
+    return new Appointment({
+      customer: "John Doe",
+      startsAt,
+      endsAt,
+    })
+  }).toThrow()
+})
+
+test("Cannot create an appointment with start date before than today", () => {
+  const startsAt = new Date()
+  const endsAt = new Date()
+
+  startsAt.setDate(startsAt.getDate() - 1)
+  endsAt.setDate(endsAt.getDate() + 3)
 
   expect(() => {
     return new Appointment({
